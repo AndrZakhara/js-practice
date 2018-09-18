66cbc1864781790dc02ad9e2b956be9fc6d45724
AndrZakhara <art.pruttov@gmail.com>
2018-09-18 17:21:58 +0300

    ajax/websocket_chat: server create

--- ajax/websocket_chat/server.js
+++ ajax/websocket_chat/server.js
@@ -1,10 +1,26 @@
 var app = require('express')();
-var http = require('http').Server(app);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+// var users = [];
+// var connections = [];
 
-app.get('/', function (req, res) {
+
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function () {
+io.on('connection', function(socket) {
+    // connections.push(socket);
+    console.log('user connected');
+    //Disconnect
+    socket.on('disconnect', function() {
+            console.log('user disconnected')
+        })
+        // connections.splice(connections.indexOf(socket), 1);
+        // console.log('Disconnected: %s sockets connected');
+})
+
+server.listen(3000, function() {
     console.log('Server started on: http://localhost:3000')
 });
