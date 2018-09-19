9685c63653e71d9fcecfe328be8bed7895564ae7
AndrZakhara <art.pruttov@gmail.com>
2018-09-19 12:28:56 +0300

    websocket_chat: fix index.html file

--- ajax/websocket_chat/server.js
+++ ajax/websocket_chat/server.js
@@ -1,25 +1,34 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
+// var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 // var users = [];
 // var connections = [];
 
 
+app.use(express.static("public"));
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
+// });
+
+// app.get('/style.css', function(req, res) {
+//     res.sendFile(__dirname + '/styles.css');
+// });
+
 
 io.on('connection', function(socket) {
-    // connections.push(socket);
     console.log('user connected');
-    //Disconnect
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+    });
+
     socket.on('disconnect', function() {
-            console.log('user disconnected')
-        })
-        // connections.splice(connections.indexOf(socket), 1);
-        // console.log('Disconnected: %s sockets connected');
-})
+        console.log('user disconnected')
+    });
+});
 
 server.listen(3000, function() {
     console.log('Server started on: http://localhost:3000')
