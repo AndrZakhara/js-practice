2b83e6e992be72d9f8944a9ea43435cc6e259676
AndrZakhara <art.pruttov@gmail.com>
2018-11-21 23:09:09 +0200

    rewrite events handler, fix styles

--- JS_basics/slider_01/script.js
+++ JS_basics/slider_01/script.js
@@ -1,34 +1,44 @@
-const galleryImg = document.getElementById('slider').getElementsByTagName('img');
-const btnLeft = document.getElementById('slider-btn-left');
-const btnRight = document.getElementById('slider-btn-right');
-console.log(galleryImg);
-console.log(galleryImg.length);
+const galleryImg = document.querySelectorAll('#slider > img');
+const btnGallery = document.querySelectorAll('#slider-nav > input');
+
 let i = 0;
 
-btnRight.onclick = () => {
+function btnOnclick () {
+  const btnType = this.getAttribute('data-value');
 
-  if(i >= galleryImg.length-1) {
-    galleryImg[i].style.display = 'none';
-    i = 0;
-    galleryImg[i].style.display = 'block';
-  } else {
-    galleryImg[i].style.display = 'none';
-    galleryImg[i+1].style.display = 'block';
-    i++;
+  if (btnType === 'left') {
+
+    if(i <= 0) {
+      galleryImg[i].style.display = 'none';
+      i = galleryImg.length - 1;
+      galleryImg[i].style.display = 'block';
+    }
+    else {
+      galleryImg[i].style.display = 'none';
+      galleryImg[i-1].style.display = 'block';
+      i--;
+    }
   }
-};
+  else {
 
-btnLeft.onclick = () => {
-
-  if(i <= 0) {
-    galleryImg[i].style.display = 'none';
-    i = galleryImg.length - 1;
-    galleryImg[i].style.display = 'block';
-  } else {
-    galleryImg[i].style.display = 'none';
-    galleryImg[i-1].style.display = 'block';
-    i--;
+    if(i >= galleryImg.length-1) {
+      galleryImg[i].style.display = 'none';
+      i = 0;
+      galleryImg[i].style.display = 'block';
+    }
+    else {
+      galleryImg[i].style.display = 'none';
+      galleryImg[i+1].style.display = 'block';
+      i++;
+    }
   }
-};
+}
+
+btnGallery.forEach((elem) => {
+  elem.onclick = btnOnclick;
+});
+
+
+
 
 
